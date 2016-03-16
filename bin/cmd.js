9c581c87361abebe978b90492fa791eb59f68457
nichoth <nichoth@gmail.com>
2016-03-15 21:56:47 -0700

    lils

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,18 +4,23 @@ var csv = require('csv-parser');
 var xtend = require('xtend');
 var map = require('map-stream');
 var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2));
 var util = require('moltin-util')({
   publicId: process.env.PUBLIC_ID,
   secretKey: process.env.SECRET_KEY
 });
 var CreateProducts = require('../');
-
 var createProducts = CreateProducts(util, __dirname+'/../example/4-rows.csv');
+var opts = {};
+
+if (args.config) {
+  opts = JSON.parse(fs.readFileSync(args.config));
+}
 
 util.request(util.endpoints.CATEGORIES)
   .then(resp => {
     var cats = resp.result;
-    createProducts(cats, { tax_band: '1202285736968061337' }, function allDone(err, resp) {
+    createProducts(cats, opts, function allDone(err, resp) {
       if (err) return console.log('error', err.response.body);
       console.log('all done', resp);
     });
--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var eToM = require('./etsy-to-moltin');
 var csv = require('csv-parser');
 var map = require('map-stream');
 
-var logStream = map((d,n) => { console.log(d); n(null, d); });
-
 module.exports = function(util, file) {
 
   return function uploadProduct(categories, productOpts, done) {
@@ -15,18 +13,6 @@ module.exports = function(util, file) {
       .pipe(csv())
       .pipe(eToM(categories, productOpts))
       .pipe(concat(function gotData(rows) {
-        // Promise.all(rows[0].images.map(img => util.fetchImage(img)))
-        //   .then(imgs => {
-        //     return Promise.all(imgs.map(img => util.resize(600, img)))
-        //   })
-        //   .then(imgs => {
-        //     return util.createProduct(rows[0].product, imgs);
-        //   })
-        //   .then(product => {
-        //     done(null, product);
-        //   })
-        //   .catch(err => done(err));
-        // ;
 
         // limit concurrency
         Promise.all(rows.map(throat(5, r => {
@@ -39,53 +25,9 @@ module.exports = function(util, file) {
           .catch(err => done(err));
         ;
 
-        // async.series(rows.map(r => uploadProduct.bind(null, m, r)),
-        //   function allDone(err, res) {
-        //     console.log('all done', arguments);
-        //   }
-        // );
       }))
     ;
   };
 
 };
-
-
-// function uploadProducts(m, cats) {
-//   fs.createReadStream('./4-rows.csv')
-//     .pipe(csv())
-//     .pipe(eToM(cats, { tax_band: '1202285736968061337' }))
-//     .pipe(concat(function gotData(rows) {
-//       async.series(rows.map(r => uploadProduct.bind(null, m, r)),
-//         function allDone(err, res) {
-//           console.log('all done', arguments);
-//         }
-//       );
-//     }))
-//     .on('error', console.log.bind(console, 'errrr'))
-//   ;
-// }
-//
-// function uploadProduct(moltin, data, next) {
-//   moltin.Product.Create(data.product, function(resp) {
-//     createImages(resp, data.images, next)
-//   }, function(err) {
-//     console.log('error', arguments);
-//     next(err);
-//   });
-// }
-//
-// function createImages(product, images, done) {
-//   return Promise.all(images.map((url, i) => {
-//     return util.fetchImage(url)
-//       .then(util.resize.bind(util, 600))
-//       .then(util.createImage.bind(util, {
-//         name: product.slug+i,
-//         assign_to: product.id
-//       }))
-//   }))
-//     .then(done.bind(null, null))
-//     .catch(done.bind(null))
-//   ;
-// }
 
