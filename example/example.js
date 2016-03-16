8201d5f3a4ebe5f91ad4ea2278b148494cba909e
nichoth <nichoth@gmail.com>
2016-03-16 13:55:01 -0700

    organize everything

--- example/example.js
+++ example/example.js
@@ -1,29 +1,34 @@
+require('dotenv').config();
+var fs = require('fs');
 var csv = require('csv-parser');
 var client = require('moltin-util')({
   publicId: process.env.PUBLIC_ID,
   secretKey: process.env.SECRET_KEY
 });
 
-var uploader = require('etsy-to-moltin');
+var opts = require('./product-opts.json');
+var uploader = require('../');
 
 client.request(client.endpoints.CATEGORIES)
   .then(resp => resp.result)
   .then(categories => {
 
-    fs.createReadStream('file.csv')
+    fs.createReadStream(__dirname+'/one-product.csv')
       .pipe(csv())
-      .pipe(uploader(client, mapFn.bind(null, categories), done)
+      .pipe(uploader(client, mapFn.bind(null, categories), done))
 
   })
+  .catch(err => console.log('err', err));
 ;
 
 function mapFn(categories, etsyProduct) {
   return {
-    tax_band: '123',
-    category: uploader.findCategory(categories, etsyProduct)
+    tax_band: opts.tax_band,
+    category: uploader.findCategory(etsyProduct, categories)
   };
 }
 
 function done(err, res) {
+  if (err) return console.log('error', err, err.response.body);
   console.log('created ' + res.length + ' products');
 }
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-//var fs = require('fs');
 var throat = require('throat');
 var concat = require('concat-stream');
-var etsyToMoltin = require('./etsy-to-moltin');
-//var csv = require('csv-parser');
-//var map = require('map-stream');
-
-
+var etsyToMoltin = require('./lib/etsy-to-moltin');
 
 module.exports = moltinUploader;
 
@@ -35,30 +30,3 @@ function moltinUploader(util, mapper, done) {
 
 moltinUploader.findCategory = etsyToMoltin.findCategory;
 
-
-
-// module.exports = function(util, file) {
-//
-//   return function uploadProduct(categories, productOpts, done) {
-//     return fs.createReadStream(file)
-//       .pipe(csv())
-//       .pipe(eToM(categories, productOpts))
-//       .pipe(concat(function gotData(rows) {
-//
-//         // limit concurrency
-//         Promise.all(rows.map(throat(5, r => {
-//           return Promise.all(r.images.map(img => util.fetchImage(img)))
-//             .then(imgs => Promise.all(imgs.map(img => util.resize(600, img))))
-//             .then(imgs => util.createProduct(r.product, imgs))
-//           ;
-//         })))
-//           .then(products => done(null, products))
-//           .catch(err => done(err));
-//         ;
-//
-//       }))
-//     ;
-//   };
-//
-// };
-//
